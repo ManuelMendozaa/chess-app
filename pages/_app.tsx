f095fe1d4c753d03a78afcd5c3ddd8f2ac4bb4ca
manuelmendoza14 <manuel@avilatek.dev>
2023-01-06 01:03:10 -0400

    images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import React from 'react';
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 import '../styles.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Chess App</title>
+        <meta name="description" content="Chess app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
